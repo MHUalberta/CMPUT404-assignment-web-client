56f56949386576c7b0cbf1b215e70bd3f89b38ba
hammad@ualberta.ca <mohammadhammad50@gmail.com>
2023-02-10 14:42:42 -0700

    Finish Assignment

--- httpclient.py
+++ httpclient.py
@@ -41,19 +41,27 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        header = self.get_headers(data)
+        code = int(re.findall("HTTP/1\.[0|1] ([0-9]+)", header)[0])  #Extract the code, for some reason also getting HTTP 1.0 responses so I have to account for that with a [0|1] in my regex
+        return code
 
-    def get_headers(self,data):
-        return None
+    def get_headers(self, data):
+        response_split = data.split("\r\n\r\n", 1)  #divide the response into [headers, body]
+        headers = response_split[0]
+        return headers
 
     def get_body(self, data):
-        return None
+        response_split = data.split("\r\n\r\n", 1)  #divide the response into [headers, body]
+        body = response_split[1] if len(response_split) > 1 else ""
+        return body
+
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
         
     def close(self):
         self.socket.close()
+
 
     # read everything from the socket
     def recvall(self, sock):
@@ -67,15 +75,52 @@ class HTTPClient(object):
                 done = not part
         return buffer.decode('utf-8')
 
+        
+    def send_request(self, url, command ,args=None):
+        #Parse URL using urllib
+        parsed_url = urllib.parse.urlparse(url)
+        host = parsed_url.hostname
+        port = 80 if (parsed_url.scheme=="http" and parsed_url.port==None) else parsed_url.port     #If port is omitted, specify port 80 (HTTP default port)
+        path = "/" if (parsed_url.path=="") else parsed_url.path                                    #If path is omitted, specify path to root
+            
+        #Connect to the host
+        self.connect(host, port)
+
+        if command == "POST":
+            #Encode the args if provided
+            data = urllib.parse.urlencode(args) if (args!=None) else ""  
+            #Build POST request
+            request = f"POST {path} HTTP/1.1\r\nHost: {host}\r\nContent-Length: {len(data)}\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n{data}"
+        
+        else:
+            #Build GET request
+            request = f"GET {path} HTTP/1.1\r\nHost: {host}\r\nConnection: close\r\n\r\n"
+        
+        self.sendall(request)
+
+
+    def handle_response(self):
+        #Wait for response and close on arrival
+        response = self.recvall(self.socket)
+        self.close()
+
+        #Parse response
+        code = self.get_code(response)
+        body = self.get_body(response)
+        return code, body
+
+
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        self.send_request(url, "GET")
+        code, body = self.handle_response()
         return HTTPResponse(code, body)
+    
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        self.send_request(url, "POST", args)
+        code, body = self.handle_response()
         return HTTPResponse(code, body)
+
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
